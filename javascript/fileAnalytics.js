e0a20dfde0eb9a98a0b42fb9f7083617ec3c462b
Craig Lyons <clyons@execproinc.com>
2015-04-07 12:40:33 -0400

    js comment style & EOL encodings

--- javascript/fileAnalytics.js
+++ javascript/fileAnalytics.js
@@ -1,1 +1,15 @@
-/* Automatically track all pdf downloads as google analytics events */jQuery.noConflict();(function($) {    $(document).ready(function() {        //Track access        var filename = $('#filename').data('filename');        _gaq.push(['_trackEvent', 'PDF', 'View', filename ]);        //TODO - add config-level option for universal analytics        //ga('send', 'event', 'pdf', 'direct', filename);        //Redirect w/ url param to bypass file process detection        window.location.replace(filename+"?processed=1");    });}(jQuery));
+/* Automatically track all pdf downloads as google analytics events */
+jQuery.noConflict();
+(function($) {
+    $(document).ready(function() {
+        /* Track access */
+        var filename = $('#filename').data('filename');
+        _gaq.push(['_trackEvent', 'PDF', 'View', filename ]);
+
+        /* TODO - add config-level option for universal analytics */
+        ga('send', 'event', 'pdf', 'direct', filename);*/
+
+        /* Redirect w/ url param to bypass file process detection */
+        window.location.replace(filename+"?processed=1");
+    });
+}(jQuery));
